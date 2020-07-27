88ba5b87cfba8e0af518378d9e810b0242315a91
Your Name <Pujabarua19>
2020-07-27 19:16:57 +0600

    updated

---
+++ resources/views/frontend/pages/about.blade.php
@@ -0,0 +1,24 @@
+@extends('frontend.layouts.index')
+@section('content')
+
+
+ <h2>About Us</h2>
+ <p>Routine and repetitive polls require a sophisticated and structured approach to the execution of the poll and the management and reporting of the poll response data. Designed well, robust and repeatable automated processes can take the grind out of handling survey datasets – improving quality and reducing cost on a daily basis. Poll Management System (PMS) is a web-enabled application which empowers the development, distributing and analysis of poll forms to improve communication, process management, and decision support across the organization. 
+A web-based poll is the collection of data through a self-administered electronic set of questions on the Web (Thomas, 2003). Web-based polls are able to conduct large-scale data collection. Web-based poll management system encompasses how the organizations organize, run and manage various types of polls through the internet networks. It lets the user not only to build questionnaires but also to publish questionnaires to the respondents. This technology provides an inexpensive mechanism for conducting  poll online instead of through traditional poll methods. Also, it speeds up the distribution and response cycles. 
+</p>
+
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+@stop
