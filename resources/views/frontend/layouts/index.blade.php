3fc799d9a65a08f1c79134c181f65608883b7c77
Your Name <Pujabarua19>
2020-08-15 12:23:19 +0600

    updated

--- resources/views/frontend/layouts/index.blade.php
+++ resources/views/frontend/layouts/index.blade.php
@@ -160,6 +160,7 @@
 
 
 <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
-    
+ <!-- @yield('scripts') -->
+   
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+//Route::get('/', function () {
+   // return view('welcome');
+//});
 Route::group(['middleware'=> 'logged'],function(){
 // Route::get('default', 'mainController@default');
 Route::get('default', 'mainController@default');
